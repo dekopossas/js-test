e03024ffcc41a5a8cde3204ab27ead986e5d042d
deko possas <andrepossaster@gmail.com>
2021-05-18 22:19:48 -0300

    rota get com lista de user fake tbm

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-// const authMiddleware = require('./auth');
+const authMiddleware = require('./auth');
 
 const router = express.Router();
 
@@ -9,9 +9,9 @@ router.post('/authenticate', (req, res) => {
   // user fake para simulação de login
   const user = {
     id: 1,
-    name: 'Mateus Silva',
-    company: 'DevAcademy',
-    website: 'http://devacademy.com.br'
+    name: 'Deko Possas',
+    company: 'Trybe',
+    website: 'https://www.linkedin.com/in/andrepossas/'
   };
 
   return res.json({
@@ -20,3 +20,27 @@ router.post('/authenticate', (req, res) => {
   })
 });
 
+// irado essa linha, não tenho certeza do uso, acredito que ele cria a rota ja testando o middleware
+router.use(authMiddleware);
+
+router.get('/users', async (req, res) => {
+  return res.json([
+    {
+      id: 1,
+      name: 'Deko Possas',
+      website: 'https://www.linkedin.com/in/andrepossas/',
+    },
+    {
+      id: 2,
+      name: 'Mark Zuckerberg',
+      website: 'https://facebook.com',
+    },
+    {
+      id: 3,
+      name: 'Bill Gates',
+      website: 'https://www.microsoft.com',
+    }
+  ])
+});
+
+module.exports = router;
